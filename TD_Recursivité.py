a07abf71f973c2a2befeb93e135636d654107ba8
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2021-10-14 11:02:06 +0200

    Add files via upload

--- TD_Recursivité.py
+++ TD_Recursivité.py
@@ -1,3 +1,4 @@
+#Exercice 1
 """
 def somme_recursif(n):
     if n == 0 :
@@ -16,8 +17,23 @@ def somme_iteratif(n):
         n = n - 1
     return f
 print(somme_iteratif(100))
-"""
+
 
 import timeit
 
 timeit.timeit('somme_recursif(1000)', number=50, setup="from __main__ import somme_recursif")
+
+#Exercice 3
+#3)
+def factorielle (n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorielle (n-1)
+"""
+#Exercice 5
+def puissance (a,n):
+    if n>=0:
+        return 0
+    else:
+    
