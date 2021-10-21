c9f14c29cfc00b52e2f195d68962a81e89fda4d2
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2021-10-21 10:54:36 +0200

    Add files via upload

--- TD_fractales.py
+++ TD_fractales.py
@@ -1,8 +1,8 @@
 import turtle
 #Exerice 1
 # Q1)
-turtle.speed(500)
-
+turtle.speed()
+"""
 def dessine(n):
     if n > 0:
         turtle.forward(n)
@@ -32,3 +32,47 @@ def dessineIteratif(n):
 print(dessineIteratif(500))
 
 #Exercice 2
+def von_koch(n,l):
+    if n==0:
+        turtule.forward(l)
+    else:
+        von_koch(n-1,l/3)
+        turtle.left(60)
+        von_koch(n-1,l/3)
+        turtle.right(120)
+        von_koch(n-1,l/3)
+        turtle.left(60)
+        von_koch(n-1,l/3)
+von_koch(4,500)
+"""
+#Exercice 3
+"""
+def carre(c):
+    for i in range(4):
+        turtle.forward(c)
+        turtle.right(90)
+        turtle.forward(100)
+    for i in range(1):
+        turtle.circle(-100,steps=4)
+        turtle.right(45)
+        turtle.forward(70.7)
+        turtle.circle(-70,steps=4)
+        turtle.right(45)
+        turtle.forward(50)
+        turtle.circle(-50,steps=4)
+        turtle.right(45)
+        turtle.forward(35.4)
+        turtle.circle(-35,steps=4)
+        
+carre(100)
+"""
+#exercice4
+
+def arbre 
+
+
+
+
+
+
+
