2b5ce2acd92bcf11fefd9fe06c29a2e677ece078
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2021-11-22 14:52:36 +0100

    Add files via upload

--- TD_POO.py
+++ TD_POO.py
@@ -1,3 +1,41 @@
+"""
+import random
+class Personnage:
+
+    def __init__(self, nom , points_de_vie , force):
+        self.vie = points_de_vie
+        self.nom = nom
+        self.force = force
+        
+    def perd_vie(self, adversaire):
+        coup = random.randint(1, adversaire.force)
+        self.vie = self.vie - coup
+        return coup
+        
+bilbo = Personnage("Bilbo", 20, 3)
+gollum = Personnage("Gollum", 20 , 4)
+frodon = Personnage("Frodon", 20 , 10)
+araignee = Personnage("Araignee", 10 , 7)
+aragorn = Personnage("Aragorn", 10 , 6)
+orc = Personnage("Orc", 10 , 9)
+    
+def game(combattant1, combattant2):
+
+    while combattant1.vie > 0 and combattant2.vie > 0:
+        coup = combattant1.perd_vie(combattant2)
+        print(combattant1.nom + " perd " + str(coup) + " point de vie")
+        coup = combattant2.perd_vie(combattant1)
+        print(combattant2.nom + " perd " + str(coup) + " point de vie")
+        
+    if combattant1.vie <= 0 and combattant2.vie > 0:
+        msg = combattant2.nom + " est vainqueur, il lui reste encore " + str(combattant2.vie) + " points de vie alors que " + combattant1.nom + " est mort"
+    elif combattant2.vie <= 0 and combattant1.vie > 0:
+        msg = combattant1.nom + " est vainqueur, il lui reste encore " + str(combattant1.vie) + " points de vie alors que " + combattant2.nom + " est mort"
+    else:
+        msg = "Les deux combattants sont morts en même temps"
+    return msg
+print(game(aragorn, orc))
+"""
 import random
 class Personnage:
 
@@ -9,17 +47,16 @@ class Personnage:
         coup_critique = random.random()
         if random.random() < 0.5:
             nbPoint = 1
-            if coup_critique > 0,95
-            nbPoint = nbPoint * 5
+            if coup_critique > 0.95:
+                nbPoint = nbPoint * 5
         else:
             nbPoint = 2
-            if coup_critique > 0,95
-            nbPoint = nbPoint * 5
+            if coup_critique > 0.95:
+                nbPoint = nbPoint * 5
         self.vie = self.vie - nbPoint
         return nbPoint
 
-        
-Bogoreh = Personnage("Bogoreh", 20)
+bilbo = Personnage("Bilbo", 20)
 gollum = Personnage("Gollum", 20)
 frodon = Personnage("Frodon", 20)
 araignee = Personnage("Araignee", 10)
