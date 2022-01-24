c2ff96ab96343f07744c5dda200ca5d5c5799f15
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2022-01-24 14:46:28 +0100

    Add files via upload

--- TD_files.py
+++ TD_files.py
@@ -1,7 +1,35 @@
+from file import *
+"""
+#Exo 2
 def inverse(file):
     p=pile()
-    while file not est_vide():
+    while not f.est_vide():
         p.empile(f.defile)
-    while pilenot est_vide():
+    while not p.est_vide():
         file.enfile(p.depile)
     return file
+
+#Exo 3
+def separe (f):
+    paire=File()
+    impaire=File()
+    while not f.est_vide ():
+        x = f.defile
+    if x % 2 == 0:
+        paire.enfile (x)
+    else:
+        impaire.enfile(x)
+    return paire, impaire
+
+#Exo 4
+#Activité 1
+#2)
+f.defile
+#3)
+vérifie si la file est vide ou non 
+#4)
+help(list.pop) --> la méthode defile permet de
+"""
+
+#Activité 2
+
