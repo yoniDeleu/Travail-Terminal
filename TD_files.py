7e95ef60dad81a9eba1ae930e1bcce268777d4b6
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2022-01-27 01:24:55 +0100

    Add files via upload

--- TD_files.py
+++ TD_files.py
@@ -8,28 +8,23 @@ def inverse(file):
     while not p.est_vide():
         file.enfile(p.depile)
     return file
-
 #Exo 3
 def separe (f):
     paire=File()
     impaire=File()
     while not f.est_vide ():
-        x = f.defile
-    if x % 2 == 0:
-        paire.enfile (x)
-    else:
-        impaire.enfile(x)
+        x = f.defile()
+        if x % 2 == 0:
+            paire.enfile(x)
+        else:
+            impaire.enfile(x)
     return paire, impaire
-
-#Exo 4
-#Activité 1
-#2)
-f.defile
-#3)
-vérifie si la file est vide ou non 
-#4)
-help(list.pop) --> la méthode defile permet de
 """
-
-#Activité 2
-
+#Exo 5
+def retire_dernier(file):
+    file_rep = File()
+    file.defile()
+    while not file.est_vide():
+        if file.long > 1:
+            file_rep.enfile(file.defile())
+    return(file_rep)
