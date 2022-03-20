9209a0c7b625778ad7af39c9b31853db7349870b
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2022-03-20 22:16:17 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+# def delta(tab):
+#     t = []
+#     t.append(tab[0])
+#     for i in range(1, len(tab)):
+#         t.append(tab[i]-tab[i-1])
+#     return t
+# print(delta([1000, 800, 802, 1000, 1003]))
+
+
+class Noeud:
+    def __init__(self, g, v, d):
+        self.gauche = g
+        self.valeur = v
+        self.droit = d
+    
+    def __str__(self):
+        return str(self.valeur)
+    
+    def est_une_feuille(self):
+        '''Renvoie True si et seulement si le noeud est une feuille'''
+        return self.gauche is None and self.droit is None
+
+
+def expression_infixe(e):
+    s = ''
+    if e.gauche is not None:
+        s = '(' + s + expression_infixe(e.gauche)
+    s = s + str(e.valeur)
+    if e.droit is not None:
+        s = s + expression_infixe(e.droit) + ')'
+    return s
