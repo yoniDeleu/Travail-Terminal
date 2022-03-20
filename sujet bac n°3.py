2805133de4faa24b6f23085c3940d6e02d5c41d5
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2022-03-20 22:48:33 +0100

    Update sujet bac n°3.py

--- sujet bac n°3.py
+++ sujet bac n°3.py
@@ -1,10 +1,10 @@
-# def delta(tab):
-#     t = []
-#     t.append(tab[0])
-#     for i in range(1, len(tab)):
-#         t.append(tab[i]-tab[i-1])
-#     return t
-# print(delta([1000, 800, 802, 1000, 1003]))
+def delta(tab):
+    t = []
+    t.append(tab[0])
+    for i in range(1, len(tab)):
+        t.append(tab[i]-tab[i-1])
+    return t
+print(delta([1000, 800, 802, 1000, 1003]))
 
 
 class Noeud:
