9df0ae4000844272154208e5aa26d4b58b2d3010
yoniDeleu <70756485+yoniDeleu@users.noreply.github.com>
2022-04-07 10:29:22 +0200

    Add files via upload

---
+++ sujet bac n°9.py
@@ -0,0 +1,31 @@
+def calcul(n):
+    res  = []
+    while n != 1:
+        if n%2 == 0:
+            n = n//2
+            res.append(n)
+            if n == 1:
+                return res
+        if n%2 == 1:
+            n = n * 3+1
+            res.append(n)
+print(calcul(7))
+
+
+
+dico = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, \
+"H":8, "I":9, "J":10, "K":11, "L":12, "M":13, \
+"N":14, "O":15, "P":16, "Q":17, "R":18, "S":19, \
+"T":20, "U":21,"V":22, "W":23, "X":24, "Y":25, "Z":26}
+def est_parfait(mot) :
+    code_c = ""
+    code_a = 0
+    for c in mot :
+        code_c = code_c + str(dico[c])
+        code_a = code_a + dico[c]
+    code_c = int(code_c)
+    if code_c % code_a == 0 :
+        mot_est_parfait = True
+    else :
+        mot_est_parfait = False
+    return [code_a, code_c, mot_est_parfait]
